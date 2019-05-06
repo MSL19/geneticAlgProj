770324444c83ddcb73bccb6af2c8507377f6f36c
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-05-05 18:17:49 -0600

    Figured out angles and wind speed
    Used the get() function to return a pixel's color to determine wind speed

--- sketch2.js
+++ sketch2.js
@@ -1,7 +1,9 @@
+
+
 class windmill{
-    constructor(bounds_x, bounds_y, max_Ang){
-        this.X = Math.random()*bounds_x;
-        this.Y = Math.random()*bounds_y;
+    constructor(bounds_x, bounds_y, max_Ang, padding){
+        this.X = Math.random()*bounds_x+padding;
+        this.Y = Math.random()*bounds_y+padding;
         this.ang = (Math.random()-0.5)*max_Ang;
     }
     getX(){
@@ -18,21 +20,38 @@ let windmills = [];
 async function setup(){
    // pixelDensity(4.7); //makes it high res
     //fullScreen();
-    createCanvas(800,1000);
-  
+    createCanvas(800,800);
+    // now the first two arguments of a rect are its center point, not corner
+    ellipseMode(CENTER); // Set ellipseMode to CENTER
+
     background(50);
-    for(let i =0; i<16; i++){
-        let tempW = new windmill(800,800,0.03);
+    for(let i =0; i<10; i++){
+        let tempW = new windmill(600,600,0.5, 100);
         windmills.push(tempW);
     }
-    for(let i =0; i<16; i++){
+    for(let i =0; i<10; i++){
+      push();
         translate(windmills[i].getX(), windmills[i].getY());
         rotate(windmills[i].getAng());
-        triangle(0, 0, -15, 30, 15, 30);
+        fill(230);
+      //  triangle(0, 0, -15, 30, 15, 30);
+        ellipse(0,0,55,15);
+        rectMode(CENTER);	
+        rect(0,15,20,20);
+
+        fill(0,0,200,171);
+        rect(0,510,55,1000);
         translate(-windmills[i].getX(), -windmills[i].getY());
-        rotate(-windmills[i].getAng());
+       // rotate(-windmills[i].getAng());
+       pop();
         console.log(windmills[i].getX(), windmills[i].getY());
+        console.log(windmills[i].getAng());
     }
+    console.log(get(400, 300));
+
+    fill(get(400, 300));
+    rect(50,50,40,40);
+
   }
  async function draw(){
     
