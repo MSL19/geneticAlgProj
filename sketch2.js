d59b37bfb160318a8b746349f0e33da79181b813
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-05-05 23:23:28 -0600

    mate and mutate finished

--- sketch2.js
+++ sketch2.js
@@ -2,6 +2,11 @@
 
 class windmill{
     constructor(bounds_x, bounds_y, max_Ang, padding){
+        this.bounds_x = bounds_x;
+        this.bounds_y = bounds_y;
+        this.max_Ang = max_Ang;
+        this.padding = padding;
+        
         this.X = Math.random()*bounds_x+padding;
         this.Y = Math.random()*bounds_y+padding;
         this.ang = (Math.random()-0.5)*max_Ang;
@@ -15,8 +20,81 @@ class windmill{
     getAng(){
         return this.ang;
     }
+    mutateW(){
+        this.X = Math.random()*this.bounds_x+this.padding;
+        this.Y = Math.random()*this.bounds_y+this.padding;
+        this.ang = (Math.random()-0.5)*this.max_Ang;
+    }
+    getPower(){
+        return get(this.X, this.Y)[2];
+    }
 }
-let windmills = [];
+class design{
+    constructor(max_num){
+
+        this.num = Math.floor(Math.random()*max_num);
+        if(this.num%2!=0){
+            this.num++;
+        }
+        console.log(this.num);
+        this.windmills = [];
+        for(let i =0; i<this.num; i++){
+            let tempW = new windmill(600,600,0.5, 100);
+            this.windmills.push(tempW);
+        }
+
+    }
+    copy(windmill_arr){
+        this.windmills = windmill_arr;
+        this.num = windmill_arr.length;
+    }
+    drawDesign(){
+        for(let i =0; i<this.num; i++){
+            push();
+              translate(this.windmills[i].getX(), this.windmills[i].getY());
+              rotate(this.windmills[i].getAng());
+              fill(230);
+            //  triangle(0, 0, -15, 30, 15, 30);
+              ellipse(0,0,55,15);
+              rectMode(CENTER);	
+              rect(0,15,20,20);
+      
+              fill(0,0,200,171);
+              rect(0,510,55,1000);
+              translate(-this.windmills[i].getX(), -this.windmills[i].getY());
+             // rotate(-windmills[i].getAng());
+             pop();
+              console.log(this.windmills[i].getX(), this.windmills[i].getY());
+              console.log(this.windmills[i].getAng());
+          }
+          console.log(get(400, 300));
+      
+          fill(get(400, 300));
+          rect(50,50,40,40);
+      
+    }
+    getDNA(){
+        return this.windmills;
+    }
+    mate(partnerDes){
+        let length1 = Math.floor(Math.random()*this.windmills.length);
+        let length2 = Math.floor(Math.random()*partnerDes.getDNA.length);
+        return{
+            child1: new design(this.windmills.slice(0,length1).concat(partnerDes.getDNA.slice(length1, partnerDes.getDNA.length))),
+            child2: new design(partnerDes.getDNA().slice(0,length2).concat(this.windmills.slice(length2, this.windmills.length))) 
+        }
+    }
+    mutate(){
+        for(let i = 0; i<this.windmills.length; i++){
+            if(Math.random()>0.01){
+                this.windmills[i].mutateW();
+            }
+        }
+
+    }
+
+}
+
 async function setup(){
    // pixelDensity(4.7); //makes it high res
     //fullScreen();
@@ -25,33 +103,15 @@ async function setup(){
     ellipseMode(CENTER); // Set ellipseMode to CENTER
 
     background(50);
-    for(let i =0; i<10; i++){
-        let tempW = new windmill(600,600,0.5, 100);
-        windmills.push(tempW);
-    }
-    for(let i =0; i<10; i++){
-      push();
-        translate(windmills[i].getX(), windmills[i].getY());
-        rotate(windmills[i].getAng());
-        fill(230);
-      //  triangle(0, 0, -15, 30, 15, 30);
-        ellipse(0,0,55,15);
-        rectMode(CENTER);	
-        rect(0,15,20,20);
-
-        fill(0,0,200,171);
-        rect(0,510,55,1000);
-        translate(-windmills[i].getX(), -windmills[i].getY());
-       // rotate(-windmills[i].getAng());
-       pop();
-        console.log(windmills[i].getX(), windmills[i].getY());
-        console.log(windmills[i].getAng());
-    }
-    console.log(get(400, 300));
-
-    fill(get(400, 300));
-    rect(50,50,40,40);
-
+    let temp = new design(15);
+    temp.drawDesign();
+    let temp2 = new design(200);
+    console.log(temp2);
+    temp2.copy(temp.getDNA());
+    temp2.mutate();
+    temp2.drawDesign();
+    
+    
   }
  async function draw(){
     
